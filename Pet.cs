cf9425a1927ef8aac6630b602e82fe51d98e79cd
Darius Hammond <hammond.work.business@gmail.com>
2022-01-24 17:56:49 -0500

    ...

--- Pet.cs
+++ Pet.cs
@@ -8,5 +8,61 @@ namespace template_csharp_virtual_pet
 {
     public class Pet
     {
+
+        //testing 
+        public string PetNaming { get; set; }
+        public string PetSpecies { get; set; }
+        public double PetHunger { get; set; }
+        public double PetFun { get; set; } // Instead of using boredom, I think the user will understand fun going lower instead of bordom getting higher.
+        public double PetHealth { get; set; }
+
+
+
+        public Pet()
+        {
+        }
+
+
+        public Pet(string petnameing, string petspecies)
+        {
+            this.PetNaming = petnameing;
+            this.PetSpecies = petspecies;
+        }
+
+
+
+        public void Pet_name()
+        {
+            string Name1 = Console.ReadLine();
+            Console.WriteLine(" Name is " + Name1); 
+
+        }
+        public void Pet_species(int speciesInput) //!!!
+        {
+            string speciesType = "";
+
+            if (speciesInput == 1)
+            {
+                speciesType = "Water";
+                Console.WriteLine( " Species is " + speciesType); //--- test case To make sure everthing working.
+            }
+            else if (speciesInput == 2)
+            {
+                speciesType = "Earth";
+                Console.WriteLine(" Species is " + speciesType);
+            }
+            else if (speciesInput == 3)
+            {
+                speciesType = "Fire";
+                Console.WriteLine(" Species is " + speciesType);
+            }
+            else
+            {
+                //PLACEHOLDER
+                // MAKE A LOOP FOR THIS DARIUS!!!
+
+                // easter egg if the user input 4 -- avater the last airbander
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,64 @@ namespace template_csharp_virtual_pet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Pet virtualPet = new Pet();
+
+
+            // titlte 
+            Console.WriteLine("\t\t\t\t\t\tWelcome to\n\n\t\t\t\t\t\tPOKNOMMMM!!!\n\t\t\t\t\t\tForm ther very not so copyrited game... ");
+
+            //TEXT FOR THE MEAN MENU
+            Console.WriteLine("\n\t\t\t\t\t\tENTER GAME:-Press 1\n\t\t\t\t\t\tEXEIT GAME:-Press 2");
+            int menuInput = Convert.ToInt32(Console.ReadLine());
+
+
+ 
+
+            switch (menuInput)
+            {
+                case 1:
+                    gameSetup(virtualPet);
+                    break;
+
+                case 2:
+                    Console.WriteLine("PLACEHOLDER"); // EXIT THE GAME
+                    break;
+            }
+
+          //  Pet virtualPet = new Pet(string namingPet);
+
+
+
+            static void gameSetup(Pet virtualPet)
+            {
+
+                // Geting players name 
+                Console.Clear();
+                Console.WriteLine("Welcome to the game");
+                Console.WriteLine("I hope you enjoy the expranicne ");
+                Console.WriteLine("What is the name of your pet? ");
+                virtualPet.Pet_name();
+
+
+                // Getting players speecies
+                Console.Clear();
+                Console.WriteLine("what species is your pet");
+                Console.WriteLine(" 1.Water\n 2.Earth \n 3.FIre");
+                int speciesInput = Convert.ToInt32(Console.ReadLine());
+                virtualPet.Pet_species(speciesInput);
+
+                //----------------------------------------
+                //maybe add another method and rename game to gameSetup
+
+                Console.WriteLine("\n\nSetup complete \nPress any button to contuine...");
+                Console.ReadLine();
+            }
+
+
+
+
+
         }
     }
 }
