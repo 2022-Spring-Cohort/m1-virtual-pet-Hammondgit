30cc1c4d34a2c7cb8b62adfd95b511b2a9b39c9f
Darius Hammond <hammond.work.business@gmail.com>
2022-01-25 23:56:32 -0500

    Think Im going to rewrite the code

--- Pet.cs
+++ Pet.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace template_csharp_virtual_pet
@@ -14,6 +15,7 @@ namespace template_csharp_virtual_pet
         public string PetSpecies { get; set; }
         public double PetHunger { get; set; }
         public double PetFun { get; set; } // Instead of using boredom, I think the user will understand fun going lower instead of bordom getting higher.
+        // if fun is below  30 the user risk mental health
         public double PetHealth { get; set; }
 
 
@@ -22,47 +24,70 @@ namespace template_csharp_virtual_pet
         {
         }
 
+        
 
-        public Pet(string petnameing, string petspecies)
+        public Pet(string petnameing, string petspecies, double pethealth, double pethunger, double petfun)
         {
             this.PetNaming = petnameing;
             this.PetSpecies = petspecies;
+            this.PetHealth = pethealth;
+            this.PetHunger = pethunger;
+            this.PetFun = petfun;
         }
 
 
 
-        public void Pet_name()
+        public string Pet_name()
         {
-            string Name1 = Console.ReadLine();
-            Console.WriteLine(" Name is " + Name1); 
-
+            Console.WriteLine("What is the name of your pet? ");
+            string petnameing = Console.ReadLine();
+            
+            return this.PetNaming = petnameing;
         }
+
         public void Pet_species(int speciesInput) //!!!
         {
             string speciesType = "";
+            
 
             if (speciesInput == 1)
             {
                 speciesType = "Water";
-                Console.WriteLine( " Species is " + speciesType); //--- test case To make sure everthing working.
+                Console.WriteLine(" Species is " + speciesType); //--- test case To make sure everthing working.
+               
             }
             else if (speciesInput == 2)
             {
                 speciesType = "Earth";
                 Console.WriteLine(" Species is " + speciesType);
+              
             }
             else if (speciesInput == 3)
             {
                 speciesType = "Fire";
                 Console.WriteLine(" Species is " + speciesType);
+              
             }
+            //else if (speciesInput == 4)
+            //{
+            //    //PLACEHOLDER
+            //    //// MAKE A LOOP FOR THIS DARIUS!!!
+            //    //Console.WriteLine("Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony.\nThen everything changed when the Fire Nation attacked. ");
+            //    //Console.WriteLine("Only the Avatar, master of all four elements, could stop them. But when the world needed him most, he vanished. ");
+            //    //Console.WriteLine("\nA hundred years passed and my brother and I discovered the new Avatar, an airbender named " + Pet_name(name1) + " Aang,\nand although his airbending skills are great, he still has a lot to learn before he's ready to save anyone.");
+            //    //Console.WriteLine("\n But I believe Aang can save the world.");
+            //    //// easter egg if the user input 4 -- avater the last airbander
+            //}
             else
             {
-                //PLACEHOLDER
-                // MAKE A LOOP FOR THIS DARIUS!!!
 
-                // easter egg if the user input 4 -- avater the last airbander
+                Console.WriteLine("wait 5 sec and pls enter a vaild input");
+                Thread.Sleep(5000);
+                Program.gameSetupspecies();
+                
             }
+
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace template_csharp_virtual_pet
 {
@@ -14,56 +15,72 @@ namespace template_csharp_virtual_pet
             Console.WriteLine("\t\t\t\t\t\tWelcome to\n\n\t\t\t\t\t\tPOKNOMMMM!!!\n\t\t\t\t\t\tForm ther very not so copyrited game... ");
 
             //TEXT FOR THE MEAN MENU
-            Console.WriteLine("\n\t\t\t\t\t\tENTER GAME:-Press 1\n\t\t\t\t\t\tEXEIT GAME:-Press 2");
+            Console.WriteLine("\n\t\t\t\t\t\tENTER GAME:-Press 1\n\n\t\t\t\t\t\tTO EXEIT GAME:-Press 0 at any time  ");
             int menuInput = Convert.ToInt32(Console.ReadLine());
 
-
- 
 
             switch (menuInput)
             {
                 case 1:
-                    gameSetup(virtualPet);
+                    gameSetupname();
                     break;
 
-                case 2:
-                    Console.WriteLine("PLACEHOLDER"); // EXIT THE GAME
+                case 0:
+                    exitGame(); // EXIT THE GAME ay any time. Thinking about making a utilitie class and putting it in it.
                     break;
             }
-
-          //  Pet virtualPet = new Pet(string namingPet);
-
-
-
-            static void gameSetup(Pet virtualPet)
-            {
-
-                // Geting players name 
-                Console.Clear();
-                Console.WriteLine("Welcome to the game");
-                Console.WriteLine("I hope you enjoy the expranicne ");
-                Console.WriteLine("What is the name of your pet? ");
-                virtualPet.Pet_name();
-
-
-                // Getting players speecies
-                Console.Clear();
-                Console.WriteLine("what species is your pet");
-                Console.WriteLine(" 1.Water\n 2.Earth \n 3.FIre");
-                int speciesInput = Convert.ToInt32(Console.ReadLine());
-                virtualPet.Pet_species(speciesInput);
-
-                //----------------------------------------
-                //maybe add another method and rename game to gameSetup
-
-                Console.WriteLine("\n\nSetup complete \nPress any button to contuine...");
-                Console.ReadLine();
-            }
-
-
-
-
 
         }
+        // Geting players name 
+        public static void gameSetupname()
+        {
+            Pet virtualPet = new Pet();
+            Console.Clear();
+            Console.WriteLine("Welcome to the game");
+            Console.WriteLine("I hope you enjoy the expranicne ");
+            virtualPet.Pet_name();
+            gameSetupspecies();
+
+        }
+
+        // Getting players species
+        public static void gameSetupspecies()
+        {
+            Pet virtualPet = new Pet();
+            Console.Clear();
+            Console.WriteLine("what species is your pet");
+            Console.WriteLine(" 1.Water\n 2.Earth \n 3.FIre");
+            int speciesInput = Convert.ToInt32(Console.ReadLine());
+            virtualPet.Pet_species(speciesInput);
+
+            Console.WriteLine("\n\nSetup complete \nPress any button to contuine..."); // I dont like how this looks and works. Might change this later
+            Console.ReadLine();
+            mainGame();
+
+        }
+        public static void mainGame()
+        {
+            Pet virtualPet = new Pet();
+            Console.Clear();
+
+
+            Console.WriteLine("is this working" + virtualPet.Pet_name());
+            // Console.WriteLine("Health "  + );
+        }
+
+
+
+
+        static void exitGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Hope you enjoy the game ");
+            Console.WriteLine("Sea yea next time");
+            Thread.Sleep(5000);
+
+        }
+        // if ( )   { exitGame()} use this latter  ----------  use this later 
+
+
     }
 }
